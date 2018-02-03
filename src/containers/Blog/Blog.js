32f552d56906c8d7096e50dc25814ea105a6a468
bonanugroho <bona.nugroho@gmail.com>
2018-02-04 00:00:21 +0700

    176 Setting up and rendering routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -11,10 +12,10 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li> <a href="/">Home</a> </li>
-                            <li> <a href="/">New Post</a> </li>                        </ul>
+                            <li> <a href="/new-post">New Post</a> </li>                        </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact render={() => <h1>Home</h1>} />
             </div>
         );
     }
